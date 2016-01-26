69caec3db29ea351add9fe6ac29d10bf0258b68c
Ryan Yogan <ryanyogan@gmail.com>
2016-01-26 12:22:21 -0600

    fixed a minor style bug

--- App/Components/NoteScreen.js
+++ App/Components/NoteScreen.js
@@ -11,16 +11,26 @@ export default class NoteScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput placeholder="Untitled Note"
-          ref="title"
-          autoFocus={true}
-          onEndEditing={(text) => {this.refs.body.focus()}}
-          style={styles.title} />
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Untitled Note"
+            ref="title"
+            autoFocus={true}
+            autoCapitalize="sentences"
+            onEndEditing={(text) => {this.refs.body.focus()}}
+            style={[styles.textInput, styles.title]}
+            underlineColorAndroid="transparent"
+          />
+        </View>
 
-        <TextInput multiline={true} 
-          placeholder="Start Typing"
-          ref="body"
-          style={styles.body} />
+        <View style={styles.inputContainer}>
+          <TextInput multiline={true} 
+            placeholder="Start Typing"
+            ref="body"
+            style={[styles.textInput, styles.body]}
+            textAlignVertical="top"
+            underlineColorAndroid="transparent"
+          />
+        </View>
       </View>
     );
   }
@@ -29,14 +39,23 @@ export default class NoteScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 64
+    marginTop: 64,
+    padding: 20
   },
   title: {
     height: 40
   },
   body: {
-    flex: 1
+    height: 250
+  },
+  inputContainer: {
+    borderBottomColor: '#9E7CE3',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    marginBottom: 10
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 16,
   }
 });
