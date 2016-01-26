53d305b50fe8363262dbe078d3dfc3cbe4f6afd8
Ryan Yogan <ryanyogan@gmail.com>
2016-01-26 10:44:17 -0600

    created a more modular button to use in future

--- App/Components/SimpleButton.js
+++ App/Components/SimpleButton.js
@@ -2,15 +2,23 @@
 
 import React, {
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 
 export default class SimpleButton extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Simple Button</Text>
-      </View>
+      <TouchableOpacity onPress={this.props.onPress}>
+        <View>
+          <Text>{this.props.customText || 'Simple Button'}</Text>
+        </View>
+      </TouchableOpacity>
     );
   }
 }
+
+SimpleButton.propTypes = {
+  onPress: React.PropTypes.func.isRequired,
+  customText: React.PropTypes.string
+};
